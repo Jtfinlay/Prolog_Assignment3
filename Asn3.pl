4628630a613e7a4639a9c199900c2f457788069b
James Finlay <jtfinlay@ualberta.ca>
2015-03-20 13:04:24 -0600

    Q3 done

--- Asn3.pl
+++ Asn3.pl
@@ -1,7 +1,9 @@
-len([],0).
-len([_|T],N) :- len(T,X), N is X+1.
+% James Finlay
+% CMPUT 325 - LEC B1
+% Assignment # 3
 
-
+rmember(X,[X|_]).
+rmember(X,[H|T]) :- rmember(X,H); rmember(X,T).
 
 % Q1 - setDifference(+S1,+S2,-S3)
 setDifference([],_,[]).
@@ -12,3 +14,15 @@ setDifference([H|T1],S2,[H|R]) :- setDifference(T1,S2,R).
 swap([],[]).
 swap([A,B|L],[B,A|R]) :- swap(L,R).
 swap([A],[A]).
+
+% Q3a - rmDup(+L, -R)
+rmDup([],[]).
+rmDup([H|T],R) :- member(H,T), rmDup(T,R).
+rmDup([H|T],[H|R]) :- rmDup(T,R).
+
+% Q3b - rmAllDup(+L,-R)
+rmAllDup(A,B) :- rmAllDup(A,[],B).
+rmAllDup([],_,[]).
+rmAllDup([H|T],Acc,R) :- atom(H), rmember(H,Acc), rmAllDup(T,Acc,R).
+rmAllDup([H|T],Acc,[H|R]) :- atom(H), L=[H,Acc], rmAllDup(T,L,R).
+rmAllDup([H|T],Acc,[R1|R2]) :- rmAllDup(H,Acc,R1), rmAllDup(T,[R1|Acc],R2).
