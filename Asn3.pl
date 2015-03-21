9dd075d861fb3a72c3b5cecedb0cb2c8d4477a0c
James Finlay <jtfinlay@ualberta.ca>
2015-03-20 20:00:36 -0600

    Comments done.

--- Asn3.pl
+++ Asn3.pl
@@ -2,8 +2,6 @@
 % CMPUT 325 - LEC B1
 % Assignment # 3
 
-use_module(library(lists)).
-
 %*******************************************************************
 % rmember(+X,+L) : Recursive 'member' call that cycles all levels of
 % a multi-dimensional list.
@@ -38,24 +36,35 @@ setDifference([],_,[]).
 setDifference([H|T1],S2,R) :- member(H,S2), setDifference(T1,S2,R).
 setDifference([H|T1],S2,[H|R]) :- setDifference(T1,S2,R).
 
-% Q2 - swap(+L,-R)
+% Q2 ***************************************************************
+% swap(+L,-R): Take list, and swap every two elements. If odd
+% elements, leave last element.
+
 swap([],[]).
 swap([A,B|L],[B,A|R]) :- swap(L,R).
 swap([A],[A]).
 
-% Q3a - rmDup(+L, -R)
+% Q3a **************************************************************
+% rmDup(+L, -R): Remove duplicates from the given list.
+
 rmDup([],[]).
 rmDup([H|T],R) :- member(H,T), rmDup(T,R).
 rmDup([H|T],[H|R]) :- rmDup(T,R).
 
-% Q3b - rmAllDup(+L,-R)
+% Q3b **************************************************************
+% rmAllDup(+L,-R): Remove all duplicates from a nested list, and
+% keep the elements in the same nested positions.
+
 rmAllDup(A,B) :- rmAllDup(A,[],B).
 rmAllDup([],_,[]).
 rmAllDup([H|T],Acc,R) :- xatom(H), rmember(H,Acc), rmAllDup(T,Acc,R).
 rmAllDup([H|T],Acc,[H|R]) :- xatom(H), L=[H,Acc], rmAllDup(T,L,R).
 rmAllDup([H|T],Acc,[R1|R2]) :- rmAllDup(H,Acc,R1), rmAllDup(T,[R1|Acc],R2).
 
-% Q4 - generate(+L, +Choice, -N)
+% Q4 ***************************************************************
+% generate(+L,+Choice,-N): Find the smallest or largest number in
+% the nested list, as set  y the 'Choice' param.
+
 generate([],_,A,A).
 generate(L,Choice,N) :-
     flatten(L,L2),
@@ -65,25 +74,41 @@ generate([H|T],Choice,A,N) :-
     g_compare(A,H,Choice,A2),
     generate(T,Choice,A2,N).
 
+% g_compare(+A,+B,+Choice,-R): Comparative method for getting
+% min/max of two items.
 g_compare(A,B,smallest,A) :- A < B.
 g_compare(A,B,smallest,B).
 g_compare(A,B,largest,A)  :- A > B.
 g_compare(A,B,largest,B).
 
-% Q5 - countAll(+L,-N)
+% Q5 ***************************************************************
+% countAll(+L,-N): Count the occurrence of each element and return
+% them in a sorted pair.
+
 countAll([],A,A).
 countAll(L,N) :-
     flatten(L,L2), countAll(L2,[],T1), sort(T1,T2), reverse(T2,T3), rswap(T3,N).
 countAll([H|T],A,R) :- map(H,A,A2), countAll(T,A2,R).
 
+% map(+E,+L,-R): Push the element into the paired mapping. If elem
+% already exists, it increments the pair.
 map(E,[],[[1,E]]).
 map(H,[[I,H|_]|T],[[R,H]|T]) :- R is I+1.
 map(E,[H|T],[H|R]) :- map(E,T,R).
 
+% rswap(+L,-R): Recursively swap the order for all pairs in the
+% list
 rswap([],[]).
 rswap([H|T],[R1|R]) :- swap(H,R1), rswap(T,R).
 
-% Q6 - convert(+Term, -Result)
+% Q6 ***************************************************************
+% convert(+Term, -Result): Convert the given Term as explained in
+% the assignment. Rules are as follows:
+%   - anything between two matching q's are not changed
+%   - any e's outside of a pair of matching q's are removed
+%   - any letter outside a pair of matching q's is changed to c
+%   - an unmatched q will be left as is
+
 convert([],_,[]).
 convert(L,R) :- convert(L,false,R).
 convert([e|T],false,R) :- convert(T,false,R).
